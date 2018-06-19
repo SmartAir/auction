4e941d80f0be53df48b2acb4ce20e2a53c3ed148
Smart Xiong <smartxiong@outlook.com>
2018-06-20 00:33:25 +0800

    Update the star component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -29,7 +29,7 @@ export class Product {
 	constructor(
 		public id: number,
 		public title: string,
-		public price: number;
+		public price: number,
 		public rating: number,
 		public description: string,
 		public category: Array<string>
