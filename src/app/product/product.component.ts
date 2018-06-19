b8462f7c6c5b8e5409410dcae38f7b482564b8b4
Smart Xiong <smartxiong@outlook.com>
2018-06-19 15:20:04 +0800

    Add product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  private products: Array<Product>;
+
   constructor() { }
 
   ngOnInit() {
+  	this.products = [
+  		new Product(1, 'Product 1', 1.99, 3, "Auction product 1", ['electronics', 'hardware']),
+  		new Product(2, 'Product 2', 2.99, 4, "Auction product 2", ['hardware']),
+  		new Product(3, 'Product 3', 3.99, 5, "Auction product 3", ['electronics']),
+  		new Product(4, 'Product 4', 4.99, 3.5, "Auction product 4", ['book']),
+  		new Product(5, 'Product 5', 5.99, 4.5, "Auction product 5", ['food']),
+  		new Product(6, 'Product 6', 6.99, 5, "Auction product 6", ['clothes']),
+  		new Product(7, 'Product 7', 7.99, 3.5, "Auction product 7", ['furniture'])
+  	];
   }
 
+}
+
+export class Product {
+	constructor(
+		public id: number,
+		public title: string,
+		public price: number;
+		public rating: number,
+		public description: string,
+		public category: Array<string>
+	) {
+
+	}
 }
