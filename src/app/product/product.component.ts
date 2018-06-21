ee3e765c4d83d3f3fe089a9e3ce3db9e7020027e
Smart Xiong <smartxiong@outlook.com>
2018-06-21 13:39:35 +0800

    Add router for product details

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -13,13 +13,13 @@ export class ProductComponent implements OnInit {
 
   ngOnInit() {
   	this.products = [
-  		new Product(1, 'Product 1', 1.99, 3, "Auction product 1", ['electronics', 'hardware']),
-  		new Product(2, 'Product 2', 2.99, 4, "Auction product 2", ['hardware']),
-  		new Product(3, 'Product 3', 3.99, 5, "Auction product 3", ['electronics']),
-  		new Product(4, 'Product 4', 4.99, 3.5, "Auction product 4", ['book']),
-  		new Product(5, 'Product 5', 5.99, 4.5, "Auction product 5", ['food']),
-  		new Product(6, 'Product 6', 6.99, 5, "Auction product 6", ['clothes']),
-  		new Product(7, 'Product 7', 7.99, 3.5, "Auction product 7", ['furniture'])
+  		new Product(1, 'Product1', 1.99, 3, "Auction product 1", ['electronics', 'hardware']),
+  		new Product(2, 'Product2', 2.99, 4, "Auction product 2", ['hardware']),
+  		new Product(3, 'Product3', 3.99, 5, "Auction product 3", ['electronics']),
+  		new Product(4, 'Product4', 4.99, 3.5, "Auction product 4", ['book']),
+  		new Product(5, 'Product5', 5.99, 4.5, "Auction product 5", ['food']),
+  		new Product(6, 'Product6', 6.99, 5, "Auction product 6", ['clothes']),
+  		new Product(7, 'Product7', 7.99, 3.5, "Auction product 7", ['furniture'])
   	];
   }
 
